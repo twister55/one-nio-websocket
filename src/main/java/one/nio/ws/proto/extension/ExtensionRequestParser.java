bc7ea3d9de7c480db92dac9b79a95e115122029f
Vadim Yelisseyev <vadim.yelisseyev@gmail.com>
2023-01-22 04:21:22 +0300

    extension request parser fix

--- src/main/java/one/nio/ws/proto/extension/ExtensionRequestParser.java
+++ src/main/java/one/nio/ws/proto/extension/ExtensionRequestParser.java
@@ -67,9 +67,8 @@ public class ExtensionRequestParser {
         if (input == null || input.length() == 0) {
             return false;
         }
-
-        for (char c : input.toCharArray()) {
-            switch (c) {
+        for (int i = 0; i < input.length(); i++) {
+            switch (input.charAt(i)) {
                 case ',':
                 case ';':
                 case '\"':
