4c1dfb403d81eb4ef04e9973d892feb6b62f01bb
Vadim Yelisseyev <vadim.yelisseyev@gmail.com>
2023-01-22 04:19:42 +0300

    writing frames code simplification

--- src/main/java/one/nio/ws/proto/message/MessageWriter.java
+++ src/main/java/one/nio/ws/proto/message/MessageWriter.java
@@ -40,34 +40,22 @@ public class MessageWriter {
     private byte[] serializeHeader(int rsv, Opcode opcode, byte[] payload) {
         int len = payload.length < 126 ? 2 : payload.length < 65536 ? 4 : 10;
         byte[] header = new byte[len];
-        byte b = 0;
-
-        b -= 128; // set the fin bit
-        b += (rsv << 4);
-        b += opcode.value;
-        header[0] = b;
-        b = 0;
-
+        header[0] = (byte) (0x80 | (rsv << 4) | opcode.value);
         // Next write the mask && length length
         if (payload.length < 126) {
-            header[1] = (byte) (payload.length | b);
+            header[1] = (byte) (payload.length);
         } else if (payload.length < 65536) {
-            header[1] = (byte) (126 | b);
+            header[1] = (byte) 126;
             header[2] = (byte) (payload.length >>> 8);
             header[3] = (byte) (payload.length & 0xFF);
         } else {
             // Will never be more than 2^31-1
-            header[1] = (byte) (127 | b);
-            header[2] = (byte) 0;
-            header[3] = (byte) 0;
-            header[4] = (byte) 0;
-            header[5] = (byte) 0;
+            header[1] = (byte) 127;
             header[6] = (byte) (payload.length >>> 24);
             header[7] = (byte) (payload.length >>> 16);
             header[8] = (byte) (payload.length >>> 8);
-            header[9] = (byte) (payload.length & 0xFF);
+            header[9] = (byte) payload.length;
         }
-
         return header;
     }
 }
