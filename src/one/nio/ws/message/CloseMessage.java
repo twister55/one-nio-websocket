b9e2dbc5143f8e00face31cf96ebf76358d18918
Vadim Yelisseyev <vadim.yelisseyev@gmail.com>
2023-01-22 04:09:04 +0300

    close message codes is final now

--- src/one/nio/ws/message/CloseMessage.java
+++ src/one/nio/ws/message/CloseMessage.java
@@ -7,21 +7,21 @@ import static one.nio.util.JavaInternals.unsafe;
  * @author <a href="mailto:vadim.yelisseyev@gmail.com">Vadim Yelisseyev</a>
  */
 public class CloseMessage extends Message<Short> {
-    public static short NORMAL = 1000;
-    public static short GOING_AWAY = 1001;
-    public static short PROTOCOL_ERROR = 1002;
-    public static short CANNOT_ACCEPT = 1003;
-    public static short RESERVED = 1004;
-    public static short NO_STATUS_CODE = 1005;
-    public static short CLOSED_ABNORMALLY = 1006;
-    public static short NOT_CONSISTENT = 1007;
-    public static short VIOLATED_POLICY = 1008;
-    public static short TOO_BIG = 1009;
-    public static short NO_EXTENSION = 1010;
-    public static short UNEXPECTED_CONDITION = 1011;
-    public static short SERVICE_RESTART = 1012;
-    public static short TRY_AGAIN_LATER = 1013;
-    public static short TLS_HANDSHAKE_FAILURE = 1015;
+    public static final short NORMAL = 1000;
+    public static final short GOING_AWAY = 1001;
+    public static final short PROTOCOL_ERROR = 1002;
+    public static final short CANNOT_ACCEPT = 1003;
+    public static final short RESERVED = 1004;
+    public static final short NO_STATUS_CODE = 1005;
+    public static final short CLOSED_ABNORMALLY = 1006;
+    public static final short NOT_CONSISTENT = 1007;
+    public static final short VIOLATED_POLICY = 1008;
+    public static final short TOO_BIG = 1009;
+    public static final short NO_EXTENSION = 1010;
+    public static final short UNEXPECTED_CONDITION = 1011;
+    public static final short SERVICE_RESTART = 1012;
+    public static final short TRY_AGAIN_LATER = 1013;
+    public static final short TLS_HANDSHAKE_FAILURE = 1015;
 
     public CloseMessage(byte[] payload) {
         this(payload.length == 0 ? null : Short.reverseBytes(unsafe.getShort(payload, byteArrayOffset)));
